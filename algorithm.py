e16c314a1908b3ced1c869752ad0de8bd96b743a
wkwu <wenkang529@163.com>
2018-03-15 17:39:06 +0800

    dayly

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,5 @@
+import time
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -90,7 +92,168 @@ class Solution(object):
             if length<index-a+1:
                 length=index-a+1
         return length
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        m=len(nums1)
+        n=len(nums2)
+        i=0
+        j=0
+        num=0
+        max=0
+        min=0
+        while True:
+           if i<m and j<n:
+               if nums1[i]<nums2[j]:
+                   i+=1
+                   num+=1
+                   min=max
+                   max=nums1[i-1]
+               else:
+                   j+=1
+                   num+=1
+                   min=max
+                   max=nums2[j-1]
+           elif i>=m:
+                j += 1
+                num += 1
+                min = max
+                max = nums2[j - 1]
+           else:
+               i += 1
+               num += 1
+               min = max
+               max = nums1[i - 1]
+           if num>=(m+n)//2+1:
+               break
+
+        if (m+n)%2==0:
+           return (max+min)/2
+        else:
+           return max+0.0
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s)<=1:
+            return s
+        r1=[]
+        for i in range(len(s)):
+                m=i-1
+                j=i
+                while m>=0 and j<len(s) and s[j]==s[m]:
+                    j+=1
+                    m-=1
+                if j-m>2 and len(r1)<j-m-1:
+                    r1=s[m+1:j]
+                m=i-2
+                j=i
+                while m>=0 and j<len(s) and s[j]==s[m]:
+                    j+=1
+                    m-=1
+                if j-m>3 and len(r1)<j-m-1:
+                    r1=s[m+1:j]
+        if r1==[]:
+            r1=s[-1]
+        return r1
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows==1:
+            return s
+        r1=''
+        skip=2*numRows-2
+        for i in range(numRows):
+            if i==0:
+                j = 0
+                while i + j < len(s):
+                    r1+=s[i + j]
+                    j += skip
+
+            if i==numRows-1:
+                j = 0
+                while i + j < len(s):
+                    r1+=s[i + j]
+                    j += skip
+
+            if i !=0 and i!=numRows-1 :
+                j=0
+                while i+j<len(s):
+                    r1+=s[i+j]
+                    if i+j+(numRows-1-i)*2<len(s):
+                        r1+=s[i+j+(numRows-1-i)*2]
+                    j+=skip
+        return r1
+    def reverse(self,x):
+        '''
+        :param x:int
+        :return: int
+        '''
+        a=10
+        r1=[]
+        r2=0
+        if x>2**31:
+            return 0
+        if x<0:
+            flag=-1
+            x=x*-1
+        else:
+            flag=1
+
+        while x!=0:
+            r2=r2*10+x%10
+            x=x//10
+        return r2*flag*(r2<2**31)
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        j = 0
+        r1 = 0
+        flag = 1
+        if len(str) == 0:
+            return 0
+        while j < len(str) and str[j] == ' ':
+            j += 1
+        if j < len(str) and str[j] in '+-':
+            flag = 1 - 2 * (str[j] == '-')
+            j += 1
+        if j < len(str):
+            for i in str[j:]:
+                if i.isdigit():
+                    r1 = r1 * 10 + int(i)
+                    j += 1
+                else:
+                    break
+        if r1 * flag > 2 ** 31 - 1:
+            return 2 ** 31 - 1
+        if r1 * flag < -(2 ** 31):
+            return -(2 ** 31)
+        return r1 * flag
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0 or (x!=0 and x%10==0):
+            return False
+        sum=0
+        while sum<x:
+            sum=sum*10+x%10
+            x=x//10
+        return (sum==x) or (x==sum//10)
+
+
+
 
 solution=Solution()
-r1=solution.lengthOfLongestSubstring('abcabcdbtvtyyyyuuiutytb')
+r1=solution.isPalindrome(12021)
 print(r1)
--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-dict={}
 
-dict['a']=1
-dict['b']=2
-dict['b']=3
-dict['c']=3
-
-print(dict['b'])
+a='a'
+print(a.isdigit())
